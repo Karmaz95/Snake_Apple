2249085af68c2b5bbac50130239a8737cf831604
Karmaz95 <karol.mazurek95@gmail.com>
2024-12-08 13:14:19 +0100

    Add printf to kext start/stop

--- X. NU/custom/HelloKext/src/HelloWorld.c
+++ X. NU/custom/HelloKext/src/HelloWorld.c
@@ -1,14 +1,17 @@
 #include <mach/mach_types.h>
+#include <libkern/libkern.h>
 
 kern_return_t HelloWorld_start(kmod_info_t * ki, void *d);
 kern_return_t HelloWorld_stop(kmod_info_t *ki, void *d);
 
 kern_return_t HelloWorld_start(kmod_info_t * ki, void *d)
 {
+    printf("HelloWorld from kext start");
     return KERN_SUCCESS;
 }
 
 kern_return_t HelloWorld_stop(kmod_info_t *ki, void *d)
 {
+    printf("HelloWorld from kext stop");
     return KERN_SUCCESS;
 }
